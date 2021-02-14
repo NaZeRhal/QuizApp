c37fb7ab45233da7e27a1d25f63e44afa25f11d7
NaZeRhal <zhanshi@yandex.ru>
2021-02-14 17:25:09 +0300

    Added quiz question activity

--- app/src/main/java/com/maxrzhe/quizapp/Constants.kt
+++ app/src/main/java/com/maxrzhe/quizapp/Constants.kt
@@ -12,8 +12,77 @@ object Constants {
             optionTwo = "USA",
             optionThree = "Mexico",
             optionFour = "China",
-            correctAnswer = 0
+            correctAnswer = 1
         )
+        val qu2 = Question(
+            id = 2,
+            question = "What country does this flag belong to?",
+            image = R.drawable.ic_flag_china,
+            optionOne = "Germany",
+            optionTwo = "Italy",
+            optionThree = "China",
+            optionFour = "Japan",
+            correctAnswer = 3
+        )
+        val qu3 = Question(
+            id = 3,
+            question = "What country does this flag belong to?",
+            image = R.drawable.ic_flag_france,
+            optionOne = "Portugal",
+            optionTwo = "France",
+            optionThree = "Sweden",
+            optionFour = "Ireland",
+            correctAnswer = 2
+        )
+        val qu4 = Question(
+            id = 4,
+            question = "What country does this flag belong to?",
+            image = R.drawable.ic_flag_sweden,
+            optionOne = "Argentina",
+            optionTwo = "Spain",
+            optionThree = "Sweden",
+            optionFour = "Germany",
+            correctAnswer = 3
+        )
+        val qu5 = Question(
+            id = 5,
+            question = "What country does this flag belong to?",
+            image = R.drawable.ic_flag_ireland,
+            optionOne = "Mexico",
+            optionTwo = "Portugal",
+            optionThree = "Sweden",
+            optionFour = "Ireland",
+            correctAnswer = 4
+        )
+
+        val qu6 = Question(
+            id = 6,
+            question = "What country does this flag belong to?",
+            image = R.drawable.ic_flag_italy,
+            optionOne = "Italy",
+            optionTwo = "France",
+            optionThree = "USA",
+            optionFour = "China",
+            correctAnswer = 1
+        )
+        val qu7 = Question(
+            id = 7,
+            question = "What country does this flag belong to?",
+            image = R.drawable.ic_flag_germany,
+            optionOne = "China",
+            optionTwo = "Ireland",
+            optionThree = "Spain",
+            optionFour = "Germany",
+            correctAnswer = 4
+        )
+
+        questionsList.add(qu1)
+        questionsList.add(qu2)
+        questionsList.add(qu3)
+        questionsList.add(qu4)
+        questionsList.add(qu5)
+        questionsList.add(qu6)
+        questionsList.add(qu7)
         return questionsList
     }
 }
--- app/src/main/java/com/maxrzhe/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/maxrzhe/quizapp/QuizQuestionsActivity.kt
@@ -1,11 +1,124 @@
 package com.maxrzhe.quizapp
 
+import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import com.maxrzhe.quizapp.databinding.ActivityQuizQuestionsBinding
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var binder: ActivityQuizQuestionsBinding
+    private lateinit var mQuestionList: List<Question>
+
+    private var mCurrentPosition: Int = 1
+    private var mSelectedOptionPosition: Int = 0
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions_actiivity)
+        binder = ActivityQuizQuestionsBinding.inflate(layoutInflater)
+
+        with(binder) {
+            val view = root
+            setContentView(view)
+            mQuestionList = Constants.getQuestions()
+
+            setQuestion()
+
+            tvOptionOne.setOnClickListener(this@QuizQuestionsActivity)
+            tvOptionTwo.setOnClickListener(this@QuizQuestionsActivity)
+            tvOptionThree.setOnClickListener(this@QuizQuestionsActivity)
+            tvOptionFour.setOnClickListener(this@QuizQuestionsActivity)
+
+            btnSubmit.setOnClickListener(this@QuizQuestionsActivity)
+        }
+
+
+    }
+
+    private fun setQuestion() {
+        with(binder) {
+
+            val question = mQuestionList[mCurrentPosition - 1]
+
+            defaultOptionView()
+
+            pbProgress.progress = mCurrentPosition
+
+            val progressText = "$mCurrentPosition / ${pbProgress.max}"
+            tvProgress.text = progressText
+
+            tvQuestionId.text = question.question
+            ivImage.setImageResource(question.image)
+            tvOptionOne.text = question.optionOne
+            tvOptionTwo.text = question.optionTwo
+            tvOptionThree.text = question.optionThree
+            tvOptionFour.text = question.optionFour
+        }
+    }
+
+    private fun defaultOptionView() {
+        val options = ArrayList<TextView>()
+        with(binder) {
+            options.add(0, tvOptionOne)
+            options.add(1, tvOptionTwo)
+            options.add(2, tvOptionThree)
+            options.add(3, tvOptionFour)
+        }
+
+        options.forEach {
+            it.setTextColor(ContextCompat.getColor(this, R.color.textHint))
+            it.typeface = Typeface.DEFAULT
+            it.background = ContextCompat.getDrawable(this, R.drawable.default_option_border_bg)
+        }
+    }
+
+    private fun selectedOptionView(tv: TextView, selectedNum: Int) {
+        defaultOptionView()
+        mSelectedOptionPosition = selectedNum
+        with(tv) {
+            setTextColor(ContextCompat.getColor(this@QuizQuestionsActivity, R.color.textMain))
+            setTypeface(typeface, Typeface.BOLD)
+            background =
+                ContextCompat.getDrawable(this@QuizQuestionsActivity, R.drawable.selected_option_border_bg)
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int) {
+        with(binder) {
+            when (answer) {
+                1 -> tvOptionOne.background =
+                    ContextCompat.getDrawable(this@QuizQuestionsActivity, drawableView)
+                2 -> tvOptionTwo.background =
+                    ContextCompat.getDrawable(this@QuizQuestionsActivity, drawableView)
+                3 -> tvOptionThree.background =
+                    ContextCompat.getDrawable(this@QuizQuestionsActivity, drawableView)
+                4 -> tvOptionFour.background =
+                    ContextCompat.getDrawable(this@QuizQuestionsActivity, drawableView)
+
+            }
+        }
+
+    }
+
+    override fun onClick(v: View?) {
+        with(binder) {
+            when (v?.id) {
+                tvOptionOne.id -> selectedOptionView(tvOptionOne, 1)
+                tvOptionTwo.id -> selectedOptionView(tvOptionTwo, 2)
+                tvOptionThree.id -> selectedOptionView(tvOptionThree, 3)
+                tvOptionFour.id -> selectedOptionView(tvOptionFour, 4)
+                btnSubmit.id -> {
+                    if (mSelectedOptionPosition == 0) {
+                        mCurrentPosition++
+                    }
+                }
+            }
+        }
+
     }
 }
+
+
